3911dc1953898de7203905082add96a7b8896c9e
radhakrishna-naidu-balineni <radhakrishna-naidu.balineni@hpe.com>
2020-02-28 13:52:10 +0530

    first commit

---
+++ product-service/src/main/java/com/javatechie/Product.java
@@ -0,0 +1,16 @@
+package com.javatechie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+}
